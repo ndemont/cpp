ad11692d88e30bae110b7eed03acbc2e6ffaa9c5
ndemont <nastasia@demont.me>
2021-07-07 14:38:18 +0200

    Begin cpp

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -1,9 +1,65 @@
 #include "Account.hpp"
 
-Account::Account()
+Account::Account(int initial_deposit)
+{
+	_totalAmount = initial_deposit;
+}
+
+Account::~Account(void)
 {
 }
 
-Account::~Account()
+static int	Account::getNbAccounts(void)
 {
+	return (_nbAccounts);
+}
+
+static int	Account::getTotalAmount(void)
+{
+	return (_totalAmount);
+}
+
+static int	Account::getNbDeposits(void)
+{
+	return (_totalNbDeposits);
+}
+
+static int	Account::getNbWithdrawals(void)
+{
+	return (_nbWithdrawals);
+}
+
+static void	Account::displayAccountsInfos(void);
+{
+	cout <<	_nbAccounts << endl;
+	cout << _totalAmount << endl;
+	cout << _totalNbDeposits << endl;
+	cout << _totalNbWithdrawals << endl;
+	cout << _accountIndex << endl;
+	cout << _amount << endl;
+	cout << _nbDeposits << endl;
+	cout << _nbWithdrawals << endl;
+}
+
+void	Account::makeDeposit( int deposit )
+{
+	_totalAmount += deposit;
+	_totalNbDeposits ++;
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	_totalAmount -= withdrawal;
+	_totalNbWithdrawals ++;
+	return ((_totalAmount));
+}
+	
+int		Account::checkAmount( void ) const
+{
+	return (_totalAmount);
+}
+
+void	Account::displayStatus( void ) const;
+{
+	cout << _totalAmount << endl;
 }
