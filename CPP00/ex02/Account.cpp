a183b9f4afd9f07b9a319ef69ed759fc9c01d995
ndemont <nastasia@demont.me>
2021-07-07 15:04:02 +0200

    Update but does not compile

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -1,4 +1,7 @@
 #include "Account.hpp"
+#include <iostream>
+
+using namespace std;
 
 Account::Account(int initial_deposit)
 {
@@ -9,36 +12,32 @@ Account::~Account(void)
 {
 }
 
-static int	Account::getNbAccounts(void)
+int	Account::getNbAccounts(void)
 {
 	return (_nbAccounts);
 }
 
-static int	Account::getTotalAmount(void)
+int	Account::getTotalAmount(void)
 {
 	return (_totalAmount);
 }
 
-static int	Account::getNbDeposits(void)
+int	Account::getNbDeposits(void)
 {
 	return (_totalNbDeposits);
 }
 
-static int	Account::getNbWithdrawals(void)
+int	Account::getNbWithdrawals(void)
 {
-	return (_nbWithdrawals);
+	return (_totalNbWithdrawals);
 }
 
-static void	Account::displayAccountsInfos(void);
+void	Account::displayAccountsInfos(void)
 {
 	cout <<	_nbAccounts << endl;
 	cout << _totalAmount << endl;
 	cout << _totalNbDeposits << endl;
 	cout << _totalNbWithdrawals << endl;
-	cout << _accountIndex << endl;
-	cout << _amount << endl;
-	cout << _nbDeposits << endl;
-	cout << _nbWithdrawals << endl;
 }
 
 void	Account::makeDeposit( int deposit )
@@ -59,7 +58,7 @@ int		Account::checkAmount( void ) const
 	return (_totalAmount);
 }
 
-void	Account::displayStatus( void ) const;
+void	Account::displayStatus( void ) const
 {
 	cout << _totalAmount << endl;
 }
