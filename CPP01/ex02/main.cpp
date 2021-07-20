12ad93570a4f555ea1079b3f5a58e76dbf709f16
Nastasia Demont <nastasia@demont.me>
2021-07-20 16:31:01 +0200

    ex02 OK

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,6 +6,9 @@ int	main()
 	std::string		*ptr = &str;
 	std::string		&ref = str;
 
+	std::cout << "The string address variable: " << &str << std::endl;
+	std::cout << "The string address through pointer: " << ptr << std::endl;
+	std::cout << "The string address through reference: " << &ref << std::endl;
 	std::cout << "The string through pointer: " << *ptr << std::endl;
 	std::cout << "The string through reference: " << ref << std::endl;
 	return 0;
