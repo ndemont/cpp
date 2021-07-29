08d6a59b1e4143f2547d52f90460a92967f23538
ndemont <nastasia@demont.me>
2021-07-29 17:45:16 +0200

    CPP07

---
+++ CPP07/ex02/Array.hpp
@@ -0,0 +1,86 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+
+template< typename T >
+class	Array
+{
+	public:
+		Array<T>(void) : _array(new T[0]), _size(0) 
+		{
+			std::cout << "Default const" << std::endl;
+			_array[0] = 0;
+		}
+		Array<T>(unsigned int size) : _array(new T[size]), _size(size)
+		{
+			std::cout << "By size const" << std::endl;
+			for (unsigned int i = 0; i < _size; i++)
+				_array[i] = 0;
+		}
+		Array<T>(Array<T> const & src) : _array(new T[0])
+		{
+			std::cout << "Copy const" << std::endl;
+			*this = src;
+		}
+		virtual ~Array<T>(void)
+		{
+			std::cout << "Dest" << std::endl;
+			delete [] _array;
+		}
+
+		class IndexOutsideArrayException : public std::exception
+    	{
+        	public:
+            	IndexOutsideArrayException(void) {};
+            	virtual ~IndexOutsideArrayException(void) throw() {};
+            	virtual const char* what() const throw()
+				{
+					return "This index does not correspond to an element of the array";
+				};
+    	};
+
+		Array<T> & operator=(Array<T> const & rhs)
+		{
+			delete [] _array;
+			_array = new T[rhs.size()];
+			T * rhsArray = rhs.getArray();
+			for (unsigned int i = 0; i < rhs.size(); i++)
+				_array[i] = rhsArray[i];
+			_size = rhs.size();
+			return *this;
+		}
+
+		T	& operator[](unsigned int idx)
+		{
+			try
+			{
+				if (idx >= this->size())
+					throw Array<T>::IndexOutsideArrayException();
+				else 
+					return _array[idx];
+			}
+			catch(const std::exception & e)
+			{
+				std::cerr << e.what() << std::endl;
+			}
+			return _array[this->size()];
+		}
+		
+
+		unsigned int	size(void) const
+		{
+			return _size;
+		}
+		
+		T	*getArray(void) const
+		{
+			return _array;
+		}
+
+	private:
+		T 				*_array;
+		unsigned int	_size;
+};
+
+#endif
