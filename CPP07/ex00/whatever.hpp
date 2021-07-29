970a170ee2b842d794518416642cafe6614aa9e1
ndemont <nastasia@demont.me>
2021-07-29 15:33:33 +0200

    ex00 syntax

--- CPP07/ex00/whatever.hpp
+++ CPP07/ex00/whatever.hpp
@@ -3,13 +3,13 @@
 
 # include <iostream> 
 
-template < typename T>
+template < typename T >
 T const &	min( T const & x, T const & y)
 {
 	return ( x < y ? x : y );
 }
 
-template < typename T>
+template < typename T >
 T const &	max( T const & x, T const & y)
 {
 	return ( x > y ? x : y );
