eabc126f5fd69df250367e633904893a9ca58303
ndemont <nastasia@demont.me>
2021-08-03 10:33:22 +0200

    CPP07 ex01

--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -18,12 +18,14 @@ int		main(void)
 	float	tab2[10] = {0,1,2,3,4,5,6,7,8,9};
 
 	iter<int>(tab, len, &mult2);
-	std::cout << "tab[0] = " << tab[0] << std::endl;
-	std::cout << "tab[1] = " << tab[1] << std::endl;
+	std::cout << "Function multiple of 2" << std::endl;
+	std::cout << "tab[0] (0 * 2) = " << tab[0] << std::endl;
+	std::cout << "tab[5] (5 * 2) = " << tab[5] << std::endl;
 
 	iter<float>(tab2, len, &mult10);
-	std::cout << "tab2[0] = " << tab2[0] << std::endl;
-	std::cout << "tab2[1] = " << tab2[1] << std::endl;
+	std::cout << "Function multiple of 10" << std::endl;
+	std::cout << "tab2[0] (0 * 10) = " << tab2[0] << std::endl;
+	std::cout << "tab2[5] (5 * 10) = " << tab2[5] << std::endl;
 
 	return 0;
 }
